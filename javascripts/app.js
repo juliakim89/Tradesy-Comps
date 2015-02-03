2284b56cc27a95a9e7a2a29bc1bfd1f525b862ad
jefffis <jeff@tradesy.com>
2015-02-03 09:24:24 -0800

    More tweaks

---
+++ javascripts/app.js
@@ -0,0 +1,20 @@
+$(function(){
+	// var win = $(window);
+	// var product_gallery = $('#product-gallery');
+	// var product_gallery_top = product_gallery.offset().top;
+	// var product_gallery_left = product_gallery.offset().left;
+	// var product_left = product_gallery.offset().left;
+	// win.resize(function(){
+	// 	product_left = product_gallery.offset().left;
+	// 	console.log(product_left);
+	// });
+	// win.scroll(function(){
+	// 	var win_top = win.scrollTop();
+	// 	if(win_top>=product_gallery_top){
+	// 		product_gallery.addClass('fixed');
+	// 		product_gallery.css('left',product_left);
+	// 	}else{
+	// 		product_gallery.removeClass('fixed');
+	// 	}
+	// });
+});
