4f456c31f61f0f7cd2739f7cf086cf103ce478d0
jefffis <jeff@tradesy.com>
2015-03-05 15:23:52 -0800

    More twerks for closet view for ind item module

--- javascripts/c.js
+++ javascripts/c.js
@@ -13,60 +13,80 @@ $(function(){
   var product = $('.product'),
     product_img = $('.has-alt-img'),
     // want = product.find('span.want.action');
-    want = product.find('span.wanted');
+    want = product.find('span.wanted'),
+    drops = $('.drops nav'),
+    dropsClose = $('.drops em');
 
-    product_img.hover(function(){
-      var img = $(this).find('img'),
-        src = img.src,
-        alt_img = img.data('src');
-      img.attr('src',alt_img);
-    }, function(){
-      var img = $(this).find('img'),
-        src = img.data('o-src');
-      img.attr('src',src);
-    });
+  product_img.hover(function(){
+    var img = $(this).find('img'),
+      src = img.src,
+      alt_img = img.data('src');
+    img.attr('src',alt_img);
+  }, function(){
+    var img = $(this).find('img'),
+      src = img.data('o-src');
+    img.attr('src',src);
+  });
 
-    want.on('click', function(){
-      // var wanted = $(this).next('.wanted'),
-      //   wanted_text = wanted.text().replace('Wanted by ','');
-      //   parseInt(wanted_text,10);
-        // console.log(wanted_text, typeof wanted_text);
+  want.on('click', function(){
+    // var wanted = $(this).next('.wanted'),
+    //   wanted_text = wanted.text().replace('Wanted by ','');
+    //   parseInt(wanted_text,10);
+      // console.log(wanted_text, typeof wanted_text);
 
-        if($(this).hasClass('no-inc')){
-          return;
-        }
+      if($(this).hasClass('no-inc')){
+        return;
+      }
 
-        var wantedNum = $(this).data('want');
-        parseInt(wantedNum,10);
-        // console.log(typeof wantedNum);
+      var wantedNum = $(this).data('want');
+      parseInt(wantedNum,10);
+      // console.log(typeof wantedNum);
 
-        if($(this).hasClass('i-added')){
-          $(this).removeClass('i-added');
-          wantedNum = wantedNum;
-        }else{
-          $(this).addClass('i-added');
-          wantedNum = wantedNum + 1;
-        }
+      if($(this).hasClass('i-added')){
+        $(this).removeClass('i-added');
+        wantedNum = wantedNum;
+      }else{
+        $(this).addClass('i-added');
+        wantedNum = wantedNum + 1;
+      }
 
-        $(this).attr('data-want',wantedNum);
+      $(this).attr('data-want',wantedNum);
 
-      // if($(this).text()=='Wanted'){
-      //   $(this).text('Removing...');
-      //   wantedToWant($(this));
-      //   wanted_text--;
-      //   if(wanted_text===0){
-      //     wanted.text('');
-      //   }else{
-      //     wanted.text('Wanted by '+ wanted_text);
-      //   }
-      // }else{
-      //   $(this).text('Wanting...');
-      //   wantToWanted($(this));
-      //   wanted_text++;
-      //   wanted.text('Wanted by '+ wanted_text);
-      // }
+    // if($(this).text()=='Wanted'){
+    //   $(this).text('Removing...');
+    //   wantedToWant($(this));
+    //   wanted_text--;
+    //   if(wanted_text===0){
+    //     wanted.text('');
+    //   }else{
+    //     wanted.text('Wanted by '+ wanted_text);
+    //   }
+    // }else{
+    //   $(this).text('Wanting...');
+    //   wantToWanted($(this));
+    //   wanted_text++;
+    //   wanted.text('Wanted by '+ wanted_text);
+    // }
 
 
-    });
+  });
+
+  drops.on('click', function(e) {
+    drops.parent().parent().removeClass('show');
+    if(!$(this).parent().parent().hasClass('show')){
+      e.preventDefault();
+      e.stopPropagation();
+      $(this).parent().parent().addClass('show');
+    }
+  });
+
+  dropsClose.on('click', function() {
+    if(!!$(this).parent().parent().hasClass('show')){
+      $(this).parent().parent().removeClass('show');
+    }else{
+      drops.parent().parent().removeClass('show');
+      $(this).parent().parent().addClass('show');
+    }
+  });
 
 });
